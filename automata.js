fdeed14b42d0435c3c92a90cee2a4911a4340115
luciomurature <luciomurature@gmail.com>
2019-06-04 16:37:57 -0300

    funconando en browser, pila

--- automata.js
+++ automata.js
@@ -1,70 +1,112 @@
+function datos(e) {
+    var file = jsonFile.files[0];
+    var reader = new FileReader();
+
+    reader.readAsText(file);
+
+    reader.onload = function(e) {
+        maquina = JSON.parse(reader.result);
+    };
+
+};
+
+
+function init(){
+
+
+    let ingreso = document.getElementById('entrada').value;
 
 
 
-let maquina = require('./aut1.json');
+    if(maquina.tipo == "AFD"){
 
 
-
-let ingreso = "abababababababbbbbbbbbbbb";
-
-let estadoActual = maquina.estadoInicial;
-
-if(maquina.maquina == "AFD"){
+        let estadoActual = maquina.estadoInicial;
 
 
-
-// hacer chequeo de alfabeto
-
-
-for(let i = 0; i < ingreso.length; i++){
-    for(let j = 0; j < maquina.transiciones.length; j++){
-        if(maquina.transiciones[j].actual == estadoActual && maquina.transiciones[j].valor == ingreso[i]){
-            estadoActual = maquina.transiciones[j].proximo;
-            break;
+        for(let i = 0; i < ingreso.length; i++){
+            for(let j = 0; j < maquina.transiciones.length; j++){
+                if(maquina.transiciones[j].actual == estadoActual && maquina.transiciones[j].valor == ingreso[i]){
+                    estadoActual = maquina.transiciones[j].proximo;
+                    break;
+                }
+            }
         }
-    }
-}
 
-    for(let i = 0; i < maquina.estadosSalida.length; i++){
-        if(estadoActual == maquina.estadosSalida[i]){
-            console.log("Cadena correcta.");
-            break;
+        for(let i = 0; i < maquina.estadosSalida.length; i++){
+            if(estadoActual == maquina.estadosSalida[i]){
+                alert("Cadena correcta.");
+                break;
+            }
+            if(estadoActual != maquina.estadosSalida[i] && maquina.estadosSalida[i] == maquina.estadosSalida[maquina.estadosSalida.length - 1]){
+                alert("Cadena Incorrecta");
+                return;
+            }
         }
+
     }
 
-}
+    if(maquina.tipo == "AP"){
 
-if(maquina.maquina == "AP"){
-    
-    class pila{
+        class Pila{
 
 
-        constructor(){
-            this.items = [];
+            constructor(){
+                this.items = [];
+            }
+            push(element){
+                this.items.push(element);
+            }
+            pop() {
+
+                if (this.items.length == 0)
+                    return "Underflow";
+                return this.items.pop();
+            }
+
+            isEmpty() {
+                return this.items.length == 0;
+            }
+
+            peek(){
+                return this.items[this.items.length - 1];
+            }
         }
-        push(element){ 
-            this.items.push(element); 
-        } 
-        pop() { 
 
-            if (this.items.length == 0) 
-            return "Underflow"; 
-            return this.items.pop(); 
-        } 
+        let pila = new Pila();
+        pila.push(maquina.simboloVacio);
+        let estadoActual = maquina.estadoInicial;
 
-        isEmpty() { 
-            return this.items.length == 0; 
-        } 
 
-        peek(){ 
-        return this.items[this.items.length - 1]; 
-        } 
+        for(let i = 0; i < ingreso.length; i++){
+            for(let j = 0; j < maquina.transiciones.length; j++){
+                if(maquina.transiciones[j].actual == estadoActual && maquina.transiciones[j].tope == pila.peek() && ingreso[i] == maquina.transiciones[j].valor ){
+                    if(maquina.transiciones[j].apilar == "L"){
+                        pila.pop();
+                        if(pila.length == 1 && i == ingreso.lenght-1){  //si tiene solo el simbolo de vacio  y la letra es la ultima   
+                            for(let k = 0; k < maquina.transiciones.length; k++){
+                                if(maquina.transiciones[k].valor == "L"){
+                                    estadoActual = maquina.transiciones[k].proximo;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    else{
+                        pila.push(maquina.transiciones[j].apilar);
+                    }
+                    estadoActual = maquina.transiciones[j].proximo;
+                    break;
+                }
+            }
+        }
+
+        if(pila.isEmpty()){
+            alert("ok");
+        }
+
+
     }
-    
 
 
-    
 }
-
-
-
